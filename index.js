60e2e5353b0e2904c9896a7759d2c147c1c597b9
Ian <iandanielwells@gmail.com>
2022-12-12 16:19:15 -0500

    commit tree c7aa5c0bb1140e7c5cef25b4677d15a65c3a3181

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = (p2,p1) => {
+   
+    return p2/p1
+}
+
+const square = (parameter3) => {
+    return parameter3*parameter3
+}
+
+const add = (sum1, sum2) => {
+    return sum1+sum2
+}
